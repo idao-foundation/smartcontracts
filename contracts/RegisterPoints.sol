d8a3cbe9ebdef002d15b10ad8b6058ac45056261
alexanderem49 <alexanderem49@gmail.com>
2024-02-14 16:33:05 +0400

    Fix: new points register contract

--- contracts/RegisterPoints.sol
+++ contracts/RegisterPoints.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -18,17 +18,22 @@ contract RegisterPoints is
 {
     using ECDSA for bytes32;
 
-    // keccak256("PermissionStructure(uint256 deadline,uint256 nonce)");
-    bytes32 public constant PS_TYPEHASH =
-        0x1a58b94d08a08929568cd68a319580254aae076eccdc7df2bd2d5506572734c2;
+    bytes32 public constant CLAIM_POINTS_TYPEHASH =
+        keccak256(
+            "ClaimForecastPoints(uint256 amount,address receiver,uint256 nonce)"
+        );
 
-    uint64 public constant ADMIN_ROLE = 0;
+    string public constant NAME = "IDAO.Forecast.Contracts.RegisterPoints";
 
-    IAccessManager public manager;
+    string public constant VERSION = "1.0.0";
+
+    uint64 public constant FORECAST_POINTS_ISSUER_ROLE =
+        uint64(uint256(keccak256("FORECAST_POINTS_ISSUER_ROLE")));
+
     bool private allowUpgrade;
 
     // Mapping by used nonces for the specific sender address and the nonce.
-    mapping(address => mapping(uint256 => bool)) public nonces;
+    mapping(uint256 => bool) public isNonceUsed;
     mapping(address => uint256) private userPoints;
 
     event PointsAdded(address user, uint256 points);
@@ -36,55 +41,34 @@ contract RegisterPoints is
     /**
      * @notice Initializes the contract.
      * @param initialAuthority_ The authority that manages this contract.
-     * @param name_ The user readable name of the signing domain.
-     * @param version_ The current major version of the signing domain.
      */
-    function initialize(
-        address initialAuthority_,
-        string memory name_,
-        string memory version_
-    ) public initializer {
+    function initialize(address initialAuthority_) public initializer {
         __AccessManaged_init(initialAuthority_);
         __UUPSUpgradeable_init();
-        __EIP712_init(name_, version_);
-        manager = IAccessManager(initialAuthority_);
+        __EIP712_init(NAME, VERSION);
     }
 
     /**
      * @notice Adds the points for the users.
-     * @param _users The users addresses.
-     * @param _points The points for the users.
-     * @param _deadline The signature valid before deadline (unix timestamp).
+     * @param _user The user addresses.
+     * @param _points Amount of points to claim.
      * @param _nonce The unique random nonce.
      * @param _v the recovery byte of the signature.
      * @param _r half of the ECDSA signature pair.
      * @param _s half of the ECDSA signature pair.
      */
-    function addPoints(
-        address[] memory _users,
-        uint256[] memory _points,
-        uint256 _deadline,
+    function claimPoints(
+        address _user,
+        uint256 _points,
         uint256 _nonce,
         uint8 _v,
         bytes32 _r,
         bytes32 _s
     ) external {
-        uint256 usersCount = _users.length;
+        permit(_points, _user, _nonce, _v, _r, _s);
+        userPoints[_user] += _points;
 
-        if (usersCount == 0) {
-            revert DataLengthsIsZero();
-        }
-        if (usersCount != _points.length) {
-            revert DataLengthsMismatch();
-        }
-
-        permit(_deadline, _nonce, _v, _r, _s);
-
-        for (uint256 i = 0; i < usersCount; i++) {
-            userPoints[_users[i]] += _points[i];
-
-            emit PointsAdded(_users[i], _points[i]);
-        }
+        emit PointsAdded(_user, _points);
     }
 
     /**
@@ -93,6 +77,20 @@ contract RegisterPoints is
      */
     function pointsBalance(address _user) external view returns (uint256) {
         return userPoints[_user];
+    }
+
+    function batchIsNonceUsed(uint256[] calldata _nonces)
+        external
+        view
+        returns (bool[] memory)
+    {
+        bool[] memory results = new bool[](_nonces.length);
+
+        for (uint256 i = 0; i < _nonces.length; i++) {
+            results[i] = isNonceUsed[_nonces[i]];
+        }
+
+        return results;
     }
 
     function upgradeToAndCall(
@@ -114,60 +112,66 @@ contract RegisterPoints is
 
     /**
      * @notice Permits the transaction with the signature.
-     * @param _deadline The signature valid before deadline (unix timestamp).
-     * @param _nonce The unique random nonce.
+     * @param _amount Amount of points issued.
+     * @param _receiver The receiver address.
+     * @param _nonce The unique nonce.
      * @param _v the recovery byte of the signature.
      * @param _r half of the ECDSA signature pair.
      * @param _s half of the ECDSA signature pair.
      */
     function permit(
-        uint256 _deadline,
+        uint256 _amount,
+        address _receiver,
         uint256 _nonce,
         uint8 _v,
         bytes32 _r,
         bytes32 _s
     ) private {
-        if (block.timestamp > _deadline) {
-            revert ExpiredDeadline();
-        }
         // Returns the hash of the type.
-        bytes32 structHash = _hash(_deadline, _nonce);
+        bytes32 structHash = _hash(_amount, _receiver, _nonce);
         // Returns the hash of the fully encoded EIP712 message for this domain.
         bytes32 typedHash = _hashTypedDataV4(structHash);
         // Returns the address that signed a hashed message.
         address recovered = ECDSA.recover(typedHash, _v, _r, _s);
 
-        (bool success, ) = manager.hasRole(ADMIN_ROLE, recovered);
+        (bool success, ) = IAccessManager(authority()).hasRole(
+            FORECAST_POINTS_ISSUER_ROLE,
+            recovered
+        );
 
         if (!success) {
             revert InvalidSignature();
         }
 
-        _useNonce(msg.sender, _nonce);
+        _useNonce(_nonce);
     }
 
     /**
      * @notice Marks the nonce as used.
-     * @param _from The sender address.
      * @param _nonce The unique random nonce.
      */
-    function _useNonce(address _from, uint256 _nonce) private {
-        if (nonces[_from][_nonce]) {
+    function _useNonce(uint256 _nonce) private {
+        if (isNonceUsed[_nonce]) {
             revert InvalidNonce();
         }
 
-        nonces[_from][_nonce] = true;
+        isNonceUsed[_nonce] = true;
     }
 
     /**
      * @notice Returns the hash of the type.
-     * @param _deadline The signature valid before deadline (unix timestamp).
-     * @param _nonce The unique random nonce.
+     * @param _amount Amount of points issued.
+     * @param _receiver The receiver address.
+     * @param _nonce The unique nonce.
      */
     function _hash(
-        uint256 _deadline,
+        uint256 _amount,
+        address _receiver,
         uint256 _nonce
     ) private pure returns (bytes32) {
-        return keccak256(abi.encode(PS_TYPEHASH, _deadline, _nonce));
+        return
+            keccak256(
+                abi.encode(CLAIM_POINTS_TYPEHASH, _amount, _receiver, _nonce)
+            );
     }
 }
